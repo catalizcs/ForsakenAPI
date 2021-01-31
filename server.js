cff871647831e0a284d2002424b38008d4e63397
catalizcs <rfechinonguyen@gmail.com>
2021-01-31 21:26:58 +0700

    fix https

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 4000;
+const port = process.env.PORT;
 const fs = require('fs')
 const path = require('path');
 const cdata = require('./modules/getcdata');
